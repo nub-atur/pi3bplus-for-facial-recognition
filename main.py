9af7071d2dbe94e82d6565f128e180d92f697c3d
nub-atur <136115054+nub-atur@users.noreply.github.com>
2023-06-13 13:51:37 +0700

    Update main.py

--- main.py
+++ main.py
@@ -15,16 +15,14 @@ encodingsP = "encodings.pickle"
 #use this xml file
 cascade = "haarcascade_frontalface_default.xml"
 
-# load the known faces and embeddings along with OpenCV's Haar
-# cascade for face detection
+# load the known faces and embeddings along with OpenCV's Haarcascade for face detection
 print("[INFO] loading encodings + face detector...")
 data = pickle.loads(open(encodingsP, "rb").read())
 detector = cv2.CascadeClassifier(cascade)
 
-# initialize the video stream and allow the camera sensor to warm up
+# initialize the video stream and allow the camera to warm up
 print("[INFO] starting video stream...")
-vs = VideoStream(src=0).start()
-# vs = VideoStream(usePiCamera=True).start()
+vs = VideoStream(usePiCamera=True).start()
 time.sleep(2.0)
 
 # start the FPS counter
@@ -32,20 +30,16 @@ fps = FPS().start()
 
 # loop over frames from the video file stream
 while True:
-	# grab the frame from the threaded video stream and resize it
-	# to 500px (to speedup processing)
+	# grab the frame from the threaded video stream and resize it to 500px (to speedup processing)
 	frame = vs.read()
 	frame = imutils.resize(frame, width=500)
 	
-	# convert the input frame from (1) BGR to grayscale (for face
-	# detection) and (2) from BGR to RGB (for face recognition)
+	# convert the input frame from (1) BGR to grayscale (for face detection) and (2) from BGR to RGB (for face recognition)
 	gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 	rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
 
 	# detect faces in the grayscale frame
-	rects = detector.detectMultiScale(gray, scaleFactor=1.1, 
-		minNeighbors=5, minSize=(30, 30),
-		flags=cv2.CASCADE_SCALE_IMAGE)
+	rects = detector.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=5, minSize=(30, 30), flags=cv2.CASCADE_SCALE_IMAGE)
 
 	# OpenCV returns bounding box coordinates in (x, y, w, h) order
 	# but we need them in (top, right, bottom, left) order, so we
@@ -66,28 +60,23 @@ while True:
 
 		# check to see if we have found a match
 		if True in matches:
-			# find the indexes of all matched faces then initialize a
-			# dictionary to count the total number of times each face
-			# was matched
+			# find the indexes of all matched faces then initialize a dictionary to count the total number of times each face was matched
 			matchedIdxs = [i for (i, b) in enumerate(matches) if b]
 			counts = {}
 
-			# loop over the matched indexes and maintain a count for
-			# each recognized face face
+			# loop over the matched indexes and maintain a count for each recognized face face
 			for i in matchedIdxs:
 				name = data["names"][i]
 				counts[name] = counts.get(name, 0) + 1
 
-			# determine the recognized face with the largest number
-			# of votes (note: in the event of an unlikely tie Python
+			# determine the recognized face with the largest number of votes (note: in the event of an unlikely tie Python
 			# will select first entry in the dictionary)
 			name = max(counts, key=counts.get)
 			
-			#If someone in your dataset is identified, print their name on the screen
+			#If someone in your dataset is identified, print their name on the screen, and play a sound file
 			if currentname != name:
 				currentname = name
 				print(currentname)
-				#play sound
 				pygame.init()
 				pygame.mixer.music.load('sound.wav')
 				pygame.mixer.music.play()
